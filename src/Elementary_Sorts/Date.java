8bbcae0dff49044c8d09a22c53cf84b96e24c1da
bowenli <bli35@buffalo.edu>
2020-07-04 22:40:12 -0400

    add Elementary Sorts implementation.

---
+++ src/Elementary_Sorts/Date.java
@@ -0,0 +1,42 @@
+package Elementary_Sorts;
+
+public class Date implements Comparable<Date> {
+
+    private final int year;
+    private final int month;
+    private final int day;
+
+    public Date(int y, int m, int d) {
+        year = y;
+        month = m;
+        day = d;
+    }
+
+    @Override
+    public int compareTo(Date that) {
+        if (this.year < that.year) return -1;
+        if (this.year > that.year) return +1;
+        if (this.month < that.month) return -1;
+        if (this.month > that.month) return +1;
+        if (this.day < that.day) return -1;
+        if (this.day > that.day) return +1;
+        return 0;
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    private static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) return false;
+        }
+        return true;
+    }
+}
---
+++ src/Elementary_Sorts/ShellSort.java
@@ -0,0 +1,29 @@
+package Elementary_Sorts;
+
+public class ShellSort {
+
+    public static void sort(Comparable[] a) {
+        int len = a.length;
+        int h = 1;
+        while (h < len / 3) h = 3 * h + 1;
+        while (h >= 1) {
+            for (int i = h; i < len; i++) {
+                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h)
+                    exch(a, j , j - h);
+            }
+            h /= 3;
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+
+}
