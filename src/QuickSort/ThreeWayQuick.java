02cbd915dcbd1d839eea9e68f0ef259274c50942
bowenli <bli35@buffalo.edu>
2020-07-31 20:31:43 -0400

    implement quick sort and 3-way quick sort.

---
+++ src/QuickSort/ThreeWayQuick.java
@@ -0,0 +1,33 @@
+package QuickSort;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+public class ThreeWayQuick {
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi >= lo) return;
+        int lt = lo, gt = hi;
+        Comparable v = a[lo];
+        int i = lo;
+        while (i <= gt) {
+            int cmp = a[i].compareTo(v);
+            if (cmp < 0) exch(a, lt++, i++);
+            else if (cmp > 0) exch(a, i, gt--);
+            else i++;
+        }
+        sort(a, lo, lt - 1);
+        sort(a, gt + 1, hi);
+    }
+
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+}
